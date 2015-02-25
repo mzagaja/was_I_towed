c408f74a85fa35984d92be559d1ed5ed003ac944
Matt Zagaja <mzagaja@alum.wpi.edu>
2015-02-25 17:53:22 -0500

    Added SMS query for license plate in notifications controller.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,5 +7,17 @@ class NotificationsController < ApplicationController
     message = client.messages.create from: '8607856371', to: '8608838042', body: 'Learning to send SMS you are.'
     render plain: message.status
   end
- 
+
+  def receive
+    vehicle_plate = params["Body"]
+    from_number = params["From"]
+    base_url = 'https://data.hartford.gov/resource/hefc-wgp8.json?$where=vehicle_plate%20=%20\''
+    full_url = base_url + vehicle_plate + "\'"
+    car = JSON.parse(open(full_url).read)
+    if car.first.nil? 
+      message = client.messages.create from: '8607856371', to: '8608838042', body: 'No, keep looking.'
+    else
+      message = client.messages.create from: '8607856371', to: '8608838042', body: 'Shit, yes it was.'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   post 'twilio/voice' => 'twilio#voice'
   post 'notifications/notify' => 'notifications#notify'
+  post 'notifications/receive' => 'notifications#receive'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
