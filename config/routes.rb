c814d5351a9abec78437f61b2e5ca8ea46c11ba0
Matt Zagaja <mzagaja@alum.wpi.edu>
2015-02-24 23:16:11 -0500

    Added SMS notification controller.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   post 'twilio/voice' => 'twilio#voice'
+  post 'notifications/notify' => 'notifications#notify'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
