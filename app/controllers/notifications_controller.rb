c0748048bbf776b243bc7a255ef6cd4d9e7314a5
Matt Zagaja <mzagaja@alum.wpi.edu>
2015-02-26 23:10:36 -0500

    Why read docs when you can just keep trying things?

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -18,12 +18,12 @@ class NotificationsController < ApplicationController
       response = Twilio::TwiML::Response.new do |r|
         r.Message 'No, keep looking.'
       end.text
-      render plain: response
+      render plain: response.status
     else
       response = Twilio::TwiML::Response.new do |r|
         r.Message 'Shit yes it was.'
       end.text
-      render plain: response
+      render plain: response.status
     end
   end
 end
