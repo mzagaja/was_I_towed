0f8598ac04c85f404ff3f3f6ca0903e55e89f4ad
Matt Zagaja <mzagaja@alum.wpi.edu>
2015-03-13 19:53:43 -0400

    Add rake task and partial.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312005231) do
+ActiveRecord::Schema.define(version: 20150313223338) do
+
+  create_table "tows", force: :cascade do |t|
+    t.integer  "TowNum"
+    t.string   "Tow_Firm"
+    t.string   "Tow_Firm_Address"
+    t.string   "Tow_Firm_Phone"
+    t.string   "Vehicle_State"
+    t.string   "Vehicle_Year"
+    t.string   "Make"
+    t.string   "Model"
+    t.string   "Color"
+    t.string   "Tow_From_Address"
+    t.date     "Date"
+    t.time     "Time"
+    t.string   "geom"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
