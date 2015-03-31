7dd3e641c66a1e04c8a25741303fa8b23abe8fb4
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-03-30 21:13:27 -0400

    Added login system.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,19 @@ class User < ActiveRecord::Base
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
