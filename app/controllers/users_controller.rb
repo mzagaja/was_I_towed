48dda99899dd640a64d295974f0e87d101cda1f8
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-04-08 17:10:19 -0400

    Updated views and added authentication protection.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -71,5 +73,17 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :vehicle_license, :telephone_number, :password)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash.now[:alert] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
