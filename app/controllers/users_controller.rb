101bd2f7f030b5180993f2ac8bec0e248e3935d0
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-04-08 21:16:08 -0400

    Additional user authentication.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :admin_user,    only: :destroy
 
   # GET /users
   # GET /users.json
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -77,6 +79,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash.now[:alert] = "Please log in."
         redirect_to login_url
       end
@@ -85,5 +88,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401005355) do
+ActiveRecord::Schema.define(version: 20150409010629) do
 
   create_table "tows", force: :cascade do |t|
     t.integer  "TowNum"
@@ -40,10 +40,11 @@ ActiveRecord::Schema.define(version: 20150401005355) do
     t.string   "email"
     t.string   "vehicle_license"
     t.string   "telephone_number"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",            default: false
   end
 
 end
