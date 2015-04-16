d94f1299e754112d995328c04db30cc3ba55c5ce
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-04-16 15:02:17 -0400

    Add missing action mailer settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'towedcar.info'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
