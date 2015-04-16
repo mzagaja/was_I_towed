c2a4b0d5178df7dfd4826584525f325241c9dc2a
Matthew Zagaja <mzagaja@alum.wpi.edu>
2015-04-16 16:10:04 -0400

    Updated front page.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -23,45 +23,45 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-  	if password_blank?
-  	  flash.now[:danger] = "Password can't be blank"
-  	  render 'edit'
-  	elsif @user.update_attributes(user_params)
-  	  log_in @user
-  	  flash[:success] = "Password has been reset."
-  	  redirect_to @user
-  	else
-  	  render 'edit'
-  	end
+    if password_blank?
+      flash.now[:danger] = "Password can't be blank"
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
-  	
-  	def get_user
-  	   @user = User.find_by(email: params[:email])
-  	end
+    
+    def get_user
+       @user = User.find_by(email: params[:email])
+    end
 
-  	# Confirms a valid user.
-  	def valid_user
-  		unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
-  	     redirect_to root_url
-  		end
-  	end
+    # Confirms a valid user.
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+         redirect_to root_url
+      end
+    end
 
-  	def user_params
-  	  params.require(:user).permit(:password, :password_confirmation)
-  	end
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 
-  	# Returns true if password is blank.
-  	def password_blank?
-  	  params[:user][:password].blank?
-  	end
+    # Returns true if password is blank.
+    def password_blank?
+      params[:user][:password].blank?
+    end
 
-  	# Checks expiration of reset token.
-  	def check_expiration
-  	  if @user.password_reset_expired?
-  	    flash[:danger] = "Password reset has expired."
-  	    redirect_to new_password_reset_url
-  	  end
-  	end
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
 end
