ca022ff4fbfffcd6ddaa10b30b9eb0f9fb77519f
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-01-07 17:03:27 -0500

    Update No Tow Message
    This updates the not towed message to let people know there might be a delay before we know where their vehicle is.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,8 +9,10 @@ class StaticPagesController < ApplicationController
     base_url = 'https://data.hartford.gov/resource/hefc-wgp8.json?$where=vehicle_plate%20=%20\''
     full_url = base_url + vehicle_plate + "\'"
     car = JSON.parse(open(full_url).read)
-    if car.first.nil? 
-      @body = "No, keep looking."
+    if car.first.nil?
+      @body = "Unfortunately we do not know where your car is yet.
+               We might know for up to three hours from the tow.
+               If you click below we will text or e-mail you when we know."
     else
       @body = "Shit, yes it was."
       @car = car
